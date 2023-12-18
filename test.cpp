460d7a1c85dfbcba6623d1241f4b6140feb84cec
aquafina <taitrinhreal@gmail.com>
2023-12-18 16:56:26 +0700

    here is some changes in the clone repo

--- test.cpp
+++ test.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-int main(){
-    cout << "Hello world";
+int main() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+
+    for (const int& x: arr)
+        cout << x << " ";
 }
