586eca6f9ff30ec42ead1368457eab5e10275e95
aquafina <taitrinhreal@gmail.com>
2023-12-18 17:01:15 +0700

    minor change

--- test.cpp
+++ test.cpp
@@ -5,13 +5,5 @@
 using namespace std;
 
 int main() {
-    int n; cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    sort(arr.begin(), arr.end());
-
-    for (const int& x: arr)
-        cout << x << " ";
+    cout << "Hello world";
 }
